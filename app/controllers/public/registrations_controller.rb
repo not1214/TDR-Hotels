b07565297875d54a68af2dc6af4949511f04c668
not1214 <ogr.g.g.nt@gmail.com>
2021-12-07 11:25:31 +0000

    [Update]devise

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,6 +4,8 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # GET /resource/sign_up
   # def new
   #   super
@@ -38,7 +40,13 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number ])
+  end
+
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
