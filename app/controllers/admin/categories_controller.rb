c7888d33ca23e3c4e65853d3d217c9d5d7d300d4
not1214 <ogr.g.g.nt@gmail.com>
2021-12-08 08:47:25 +0000

    [Add]adminのカテゴリー

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,15 @@
 class Admin::CategoriesController < ApplicationController
   def index
+    @categories = Category.all
+    @category = Category.new
   end
 
   def edit
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "カテゴリーの名前を更新しました。"
+      redirect_to admin_categories_path
+    end
   end
+
 end
