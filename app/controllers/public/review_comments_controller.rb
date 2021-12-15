e1cce65fd6eefc63a0494961180cd4fb0fddf93c
not1214 <ogr.g.g.nt@gmail.com>
2021-12-15 16:52:25 +0000

    [Update]コメント機能

--- app/controllers/public/review_comments_controller.rb
+++ app/controllers/public/review_comments_controller.rb
@@ -1,20 +1,21 @@
 class Public::ReviewCommentsController < ApplicationController
 
   def create
-    review = Review.find(params[:id])
+    @review = Review.find(params[:review_id])
     comment = current_member.review_comments.new(review_comment_params)
-    comment.review_id = review.id
+    comment.review_id = @review.id
     comment.save
-    redirect_to hotel_review_path(review.hotel_id, review)
   end
 
   def destroy
+    @review = Review.find(params[:review_id])
+    ReviewComment.find_by(id: params[:id]).destroy
   end
 
   private
 
   def review_comment_params
-    params.require(:review_comments).permit(:comment)
+    params.require(:review_comment).permit(:comment)
   end
 
 end
