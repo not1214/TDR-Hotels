47a7e5bebcf5ca46caa8ffd5e18bbe2ed4559b67
not1214 <ogr.g.g.nt@gmail.com>
2021-12-23 11:32:24 +0000

    [Update]コントローラー微修正

--- app/controllers/admin/review_comments_controller.rb
+++ app/controllers/admin/review_comments_controller.rb
@@ -5,6 +5,7 @@ class Admin::ReviewCommentsController < ApplicationController
   def destroy
     @review = Review.find(params[:review_id])
     ReviewComment.find_by(id: params[:id]).destroy
+    flash.now[:notice] = "コメントを削除しました。"
   end
 
 end
