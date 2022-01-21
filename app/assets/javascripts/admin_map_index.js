afde728db34d8621ca3dfaf3e24d2b562c349065
not1214 <ogr.g.g.nt@gmail.com>
2022-01-21 09:02:44 +0000

    [Fix]地図修正

--- app/assets/javascripts/admin_map_index.js
+++ app/assets/javascripts/admin_map_index.js
@@ -1,48 +1,48 @@
-  let map;
-  let marker = []; // マーカーを複数表示させたいので、配列化
-  let infoWindow = []; // 吹き出しを複数表示させたいので、配列化
-  let markerData = gon.hotels; // コントローラーで定義したインスタンス変数を変数に代入
+let admin_map;
+let admin_marker = []; // マーカーを複数表示させたいので、配列化
+let admin_infoWindow = []; // 吹き出しを複数表示させたいので、配列化
+let admin_MarkerData = gon.hotels; // コントローラーで定義したインスタンス変数を変数に代入
 
-  function admin_map_index() {
-    geocoder = new google.maps.Geocoder()
+function admin_map_index() {
+  geocoder = new google.maps.Geocoder()
 
-    map = new google.maps.Map(document.getElementById('map-index'), {
-      center: { lat: 35.63676467540053, lng: 139.88400364602666 }, // 舞浜駅を中心に表示させている
-      zoom: 12,
+  admin_map = new google.maps.Map(document.getElementById('admin-map-index'), {
+    center: { lat: 35.63676467540053, lng: 139.88400364602666 }, // 舞浜駅を中心に表示させている
+    zoom: 12,
+  });
+
+  // 繰り返し処理でマーカーと吹き出しを複数表示させる
+  for (var i = 0; i < admin_MarkerData.length; i++) {
+    // console.log(admin_MarkerData[i]);
+
+    let id = admin_MarkerData[i]['id']
+
+    // 各地点の緯度経度を算出
+    markerLatLng = new google.maps.LatLng({
+      lat: admin_MarkerData[i]['latitude'],
+      lng: admin_MarkerData[i]['longitude']
     });
 
-    // 繰り返し処理でマーカーと吹き出しを複数表示させる
-    for (var i = 0; i < markerData.length; i++) {
-      // console.log(markerData[i]);
-
-      let id = markerData[i]['id']
-
-      // 各地点の緯度経度を算出
-      markerLatLng = new google.maps.LatLng({
-        lat: markerData[i]['latitude'],
-        lng: markerData[i]['longitude']
-      });
-
-      // 各地点のマーカーを作成
-      marker[i] = new google.maps.Marker({
-        position: markerLatLng,
-        map: map
-      });
-
-      // 各地点の吹き出しを作成
-      infoWindow[i] = new google.maps.InfoWindow({
-        // 吹き出しの内容
-        content: '<a href="/admin/hotels/' + id + '">' + markerData[i]['hotel_name'] +'</a>'
-      });
-
-      // マーカーにクリックイベントを追加
-      markerEvent(i);
-    }
-  }
-
-  // マーカーをクリックしたら吹き出しを表示
-  function markerEvent(i) {
-    marker[i].addListener('click', function () {
-      infoWindow[i].open(map, marker[i]);
+    // 各地点のマーカーを作成
+    admin_marker[i] = new google.maps.Marker({
+      position: markerLatLng,
+      map: admin_map
     });
+
+    // 各地点の吹き出しを作成
+    admin_infoWindow[i] = new google.maps.InfoWindow({
+      // 吹き出しの内容
+      content: '<a href="/admin/hotels/' + id + '">' + admin_MarkerData[i]['hotel_name'] +'</a>'
+    });
+
+    // マーカーにクリックイベントを追加
+    markerEvent(i);
   }
+}
+
+// マーカーをクリックしたら吹き出しを表示
+function markerEvent(i) {
+  admin_marker[i].addListener('click', function () {
+    admin_infoWindow[i].open(admin_map, admin_marker[i]);
+  });
+}
